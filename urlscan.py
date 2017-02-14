e679637a7abb9e083fb37303a47e7be4bc6abf26
Matthew J. Early <matthewjearly@gmail.com>
2017-02-14 14:16:31 -0500

    added mailto: on email matching

--- urlscan.py
+++ urlscan.py
@@ -11,7 +11,6 @@ import urllib.request                         # for urlopen
 import re                                     # for regular expressiosn
 import sys                                    # for the command line argument
 
-
 def pass_to_database(fname_lname, email, _database):
     sql_create_contactdb_table = """ CREATE TABLE IF NOT EXISTS contactdb (
                                         id integer PRIMARY KEY AUTOINCREMENT,
@@ -31,14 +30,11 @@ def pass_to_database(fname_lname, email, _database):
 
     with conn:
         # create a new project
-        # contact = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30')
-        # contact = (firstname,lastname,email)
+
         contact = prepare_contact_for_db(fname_lname, email)
 
         project_id = create_project(conn, contact)
 
-        
-            
 def create_connection(_database):
     try:
         conn = sqlite3.connect(_database)
@@ -61,25 +57,20 @@ def create_project(conn, contact):
     cur.execute(sql, contact)
     return cur.lastrowid
 
-# def create_task(conn, task):
-#     sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
-#               VALUES(?,?,?,?,?,?) '''
-#     cur = conn.cursor()
-#     cur.execute(sql, task)
-#     return cur.lastrowid
+
 
 def prepare_contact_for_db(firstandlastnames, emailaddresses):
     #make a list of firstname, lastname, email
     #and return it
     results = []
     lst = []
-    for e in range(len(emailaddresses)):
+    for e in range(len(firstandlastnames)):
         lst = firstandlastnames[e].split("\xa0", 2)
         lst.append(emailaddresses[e])
-        # print (lst)  #just to test that it is working
+        print (lst)  #just to test that it is working
         results.append(lst)
-    print (results)
 
+    # print (results)
 
     return results
 
@@ -94,34 +85,40 @@ def transfer_contacts_to_sqlitedb(_url, _database):
     
     # search for firstname&nbsp;lastname combinantion and store in a list
     first_last_names = re.findall(r"[A-Z]{1}[A-Za-z-]+\xa0[A-Z]{1}[A-Za-z-]+",s) #\xa0 is &nbsp; 
-    # change it int a set to remove duplicates then back to a list
-    set(first_last_names)
-    list(first_last_names) 
-
 
     # search for emails and store in a list
-    email_addresses = re.findall(r"[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",s)  
-    # change it into a set to remove duplicates then back to a list
-    set(email_addresses)
-    list(email_addresses)
+    email_addresses = re.findall(r"(?<=mailto:)[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",s)
+    # email_addresses = re.findall(r"[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",mailto)
+
     
     # pass this newfound data to our functon that works with the sqllite3 database
     # they should already be in order
     pass_to_database(first_last_names, email_addresses, _database)
 
 
+
+
+
+
 def main(argv):
 
+    # checks to make sure something was passed in
     if len(argv) > 1:
         print('Too many arguments')
         print('use \'urlscan.py databasename.db\'')
         sys.exit()
 
+    # this one is pretty self-explanatory
     url_to_check = "https://www.ohio.edu/engineering/about/people"
 
+    # begin our main routine
     transfer_contacts_to_sqlitedb(url_to_check, argv[0])
 
+    # finished message
     print("Work Complete.")
+
+
+
 
 
 
