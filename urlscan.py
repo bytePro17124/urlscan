181249bcdd3752756eeee43d15a776c03cba9122
Matthew J. Early <matthewjearly@gmail.com>
2017-02-15 11:22:47 -0500

    organizing for clarity

--- urlscan.py
+++ urlscan.py
@@ -11,15 +11,14 @@ import urllib.request                         # for urlopen
 import re                                     # for regular expressiosn
 import sys                                    # for the command line argument
 
-def export_to_sqlite3(fname_lname, email, _database):
-
+def export_to_sqlite3(_data, _database):
 	# create a database connection
 	try:
 		conn = sqlite3.connect(_database)
 	except sqlite3.Error as e:
 		print("Connection to database error: %s" % e)
 
-	# fire up a cursor
+	# fires up a cursor
 	c = conn.cursor()
 
 	# use the curosr to create a starting table
@@ -27,51 +26,56 @@ def export_to_sqlite3(fname_lname, email, _database):
 		c.execute('''CREATE TABLE fresh_email_contacts
 					(ID integer PRIMARY KEY, FirstName text,
 					LastName text, Email text)''')
-	except sqlite3.Error:
-		print("problem initializing the database")
+	except sqlite3.Error as e:
+		print("Problem initializing the database. Error: %s" % e)
 
-	# turn our data source into a proper list of lists ready to export
-	db_ready_list = prepare_contact_for_db(fname_lname, email)
-
-	for e in db_ready_list:
+	# iterate through our prepared data list and put the values into the table
+	for d in _data:
 		c.execute('''INSERT INTO fresh_email_contacts
 					(FirstName,LastName,Email) VALUES (?,?,?)''',\
-					(e[0],e[1],e[2]))
+					(d[0],d[1],d[2]))
 
+	# finalize our changes to the database
 	conn.commit()
 
+	# close our connection to the database file
 	conn.close()
-
-def prepare_contact_for_db(firstandlastnames, emailaddresses):
-    #make a list of firstname, lastname, email
-    #and return it
-    results = []
-    lst = []
-    for e in range(len(firstandlastnames)):
-        lst = firstandlastnames[e].split("\xa0", 2)
-        lst.append(emailaddresses[e])
-        print (lst)  #just to test that it is working
-        results.append(lst)
-    return results
+	
 
 
-def transfer_contacts_to_sqlitedb(_url, _database):
-    # open the url sepcified
-    f = urllib.request.urlopen(_url)
-    
-    # read in teh data from the url convert bytes to characters 
-    s = f.read().decode('utf-8')               
-    
-    # search for firstname&nbsp;lastname combinantion and store in a list
-    first_last_names = re.findall(r"[A-Z]{1}[.A-Za-z-]+\s*[A-Z]*.*\xa0[A-Z]{1}[A-Za-z-]+",s) #\xa0 is &nbsp; 
+def prepare_contact_for_db(_fullname, _email):
+	#make a list of firstname, lastname, email
 
-    # search for emails and store in a list
-    email_addresses = re.findall(r"(?<=mailto:)[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",s)
-    # email_addresses = re.findall(r"[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",mailto)
-    
-    # pass this newfound data to our functon that works with the sqllite3 database
-    # they should already be in order
-    export_to_sqlite3(first_last_names, email_addresses, _database)
+	results = []                                    # local placeholder to return
+	lst = []                                        # placeholder to helpo populate results
+
+	for e in range(len(_fullname)):
+		lst = _fullname[e].split("\xa0", 2) # splits first/last names
+		lst.append(_email[e])                       # puts email addy with names
+		print (lst)                                 # just to test that it is working
+		results.append(lst)                         # put this list of names/email into a new list
+
+	#and return it
+	return results
+
+
+
+def scan_site_to_db(_url, _database):
+	# open the url sepcified
+	f = urllib.request.urlopen(_url)
+	## read in the site data from the url 
+	#  convert bytes to characters 
+	s = f.read().decode('utf-8')               
+	## search for firstname&nbsp;lastname combinantion and store in a list
+	#  \xa0 is &nbsp; 
+	first_and_last_names = re.findall(r"[A-Z]{1}[.A-Za-z-]+\s*[A-Z]*.*\xa0[A-Z]{1}[A-Za-z-]+",s) 
+	## search for emails
+	#  search for re format matching after mailto:
+	email_addresses = re.findall(r"(?<=mailto:)[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}",s) 
+	#  turn it into proper formatting
+	formatted_data = prepare_contact_for_db(first_and_last_names, email_addresses)
+	#  export
+	export_to_sqlite3(formatted_data, _database)
 
 
 
@@ -88,11 +92,11 @@ def main(argv):
 	url_to_check = "https://www.ohio.edu/engineering/about/people"
 
 	# begin our main routine
-	transfer_contacts_to_sqlitedb(url_to_check, argv[0])
+	scan_site_to_db(url_to_check, argv[0])
 
 	# finished message
 	print("  \\0/")
-	print("   |     NEW EMAILS!")
+	print("   |     NEW EMAILS! PREP THE SPAM!")
 	print("  / \\")
 	print("Work Complete. See your sqlite3 database file, %s" % argv[0])
 
