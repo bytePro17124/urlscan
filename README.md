e21b0e3de1bec48e2fef5ceea3a63d7f9a78c530
Matthew J. Early <matthewjearly@gmail.com>
2017-02-15 11:04:38 -0500

    cleanup and clarity

---
+++ README.md
@@ -0,0 +1,8 @@
+# URLSCAN
+
+* Use python3 to scan a website for names and emails  
+* Export them to an sqlite3 database file  
+### To Run
+* requirements: python3 sqlite3  
+* __python3 urlscan.py mynewdatabase.db__  
+	* you can name _mynewdatabase.db_ anything you like as long as it ends in _.db_
--- urlscan.py
+++ urlscan.py
@@ -42,54 +42,6 @@ def export_to_sqlite3(fname_lname, email, _database):
 
 	conn.close()
 
-	# conn = create_connection(database)
-    # if conn is not None:
-    #     # create projects table
-    #     create_table(conn, sql_create_projects_table)
-    #     # create tasks table
-    #     create_table(conn, sql_create_tasks_table)
-    # else:
-    #     print("Error! cannot create the database connection.")
-
-    # with conn:
-    #     # create a new project
-    #     project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30')
-    #     project_id = create_project(conn, project)
-
-    #     # tasks
-    #     task_1 = ('Analyze the requirements', 1, 1, project_id, '2015-01-01', '2015-01-02')
-    #     task_2 = ('Confirm the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
-
-    #     # create tasks
-    #     create_task(conn, task_1)
-    #     create_task(conn, task_2)
-
-# def create_connection(db_file):
-#     try:
-#         conn = sqlite3.connect(db_file)
-#         return conn
-#     except Exception as e:
-#         print(e)
-#     return None
-
-# def create_table(conn, create_table_sql):
-#     try:
-#         c = conn.cursor()
-#         c.execute(create_table_sql)
-#     except Exception as e:
-#         print(e)
-
-# def create_project(conn, project):
-#     sql = ''' INSERT INTO projects(name,begin_date,end_date)
-#               VALUES(?,?,?) '''
-#     cur = conn.cursor()
-#     cur.execute(sql, project)
-#     return cur.lastrowid
-
-
-## prepare_contact_for_db Changes The Imported firstandlastnames and emailaddresses 
-## Data Into a List of Lists
-## In preparation for export to database.
 def prepare_contact_for_db(firstandlastnames, emailaddresses):
     #make a list of firstname, lastname, email
     #and return it
@@ -126,27 +78,26 @@ def transfer_contacts_to_sqlitedb(_url, _database):
 
 def main(argv):
 
-    # checks to make sure something was passed in
-    if len(argv) > 1:
-        print('Too many arguments')
-        print('use \'urlscan.py databasename.db\'')
-        sys.exit()
+	# checks to make sure something was passed in
+	if len(argv) > 1:
+		print('Too many arguments')
+		print('use \'urlscan.py databasename.db\'')
+		sys.exit()
 
-    # this one is pretty self-explanatory
-    url_to_check = "https://www.ohio.edu/engineering/about/people"
+	# this one is pretty self-explanatory
+	url_to_check = "https://www.ohio.edu/engineering/about/people"
 
-    # begin our main routine
-    transfer_contacts_to_sqlitedb(url_to_check, argv[0])
+	# begin our main routine
+	transfer_contacts_to_sqlitedb(url_to_check, argv[0])
 
-    # finished message
-    print("Work Complete.")
-
-
-
-
+	# finished message
+	print("  \\0/")
+	print("   |     NEW EMAILS!")
+	print("  / \\")
+	print("Work Complete. See your sqlite3 database file, %s" % argv[0])
 
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
